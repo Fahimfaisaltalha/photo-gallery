5e154a50988a38e761dd3fd327d53e5554caa1b5
Fahimfaisaltalha <faisal15-4721@diu.edu.bd>
2025-04-27 19:48:14 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+include 'includes/header.php';
+
+?>
+<div class="my-4">
+    <h1>Photo Gallery</h1>
+</div>
+
+<div class="row">
+    <div class="card" style="width: 18rem;">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">2025</p>
+            
+        </div>
+    </div>
+</div
+
+    <?php
+    include 'includes/footer.php';
+    ?>
